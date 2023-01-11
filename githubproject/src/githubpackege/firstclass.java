78b1c3e71852d80eec373dc4d584a4e42e6928df
amul1798 <122445933+amul1798@users.noreply.github.com>
2023-01-11 19:57:38 +0530

    modified file

--- githubproject/src/githubpackege/firstclass.java
+++ githubproject/src/githubpackege/firstclass.java
@@ -3,5 +3,8 @@ package githubpackege;
 public class firstclass {
 public static void main(String[] args) {
 	System.out.println("amul");
+System.out.println("ammanagi fair comim=ng soon");
+System.out.println("from 13th to 17th jan");
+System.out.println("welcome all");
 }
 }
