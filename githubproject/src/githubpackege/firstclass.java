cfccaf82a90649e0583b49b19713f5a56ef55e0f
amul1798 <122445933+amul1798@users.noreply.github.com>
2023-01-11 20:09:51 +0530

    a

--- githubproject/src/githubpackege/firstclass.java
+++ githubproject/src/githubpackege/firstclass.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 System.out.println("ammanagi fair comim=ng soon");
 System.out.println("from 13th to 17th jan");
 System.out.println("welcome all");
+System.out.println("jingat");
 }
 }
